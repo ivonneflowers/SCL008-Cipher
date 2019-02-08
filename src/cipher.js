712202f9f4bae3a7feea1a15a69594b7cbba81f0
Macarena Terán <macarenateran.flores@gmail.com>
2019-02-08 16:04:43 -0300

    Actualización cipher js viernes 8 febrero, faltan funciones y union a index js

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,97 @@
 window.cipher = {
-  encode: () => {
+  encode: (offset, text) => {
     /* Acá va tu código */
+    let result ="";
+      for ( let i = 0; i< text.length; i++){
+          let textToAscii= 0;
+          let texToAscii1= 0;
+          let space = "";
+          textToAscii = text.charCodeAt(i);
+          if(textToAscii===32){
+            space += 32;
+            result+= String.fromCharCode(textToAscii);
+          }
+            else if(textToAscii >= 65 && textToAscii <=90 ){
+                  textToAscii1= (textToAscii - 65 + offset) %26 + 65;
+                  result += String.fromCharCode(textToAscii1);
+             }else{
+                alert("PORFAVOR INGRESA TU TEXTO SIN SIGNOS DE PUNTUACION")
+                return result;
+              }
+          }
+return result;
   },
-  decode: () => {
+
+
+
+
+  decode: (offset, text) => {
     /* Acá va tu código */
+  let result ="";
+  for (i= 0; i< text.length; i++){
+    let textToAscii= 0;
+    let textToAscii1=0;
+    let space = "";
+    texttoascii = text.charCodeAt(i);
+    if(textToAscii=== 32){
+      space += 32;
+      result += String.fromCharCode(textToAscii);
+    }
+    else if (textToAscii >= 65 && textToAscii <= 90){
+      textToAscii1=(textToAscii - 90 - offset) %26 - 90;
+      result += String.fromCharCode(textToAscii1);
+    }else{
+      alert("PORFAVOR INGRESA TU TEXTO SIN SIGNOS DE PUNTUACION");
+      return result;
+    }
+    return result;
   }
-};
+  }
+
+
+
+
+  /*function decode (offset,text){
+  let result ="";
+  for (i= 0; i< text.length; i++){
+    let textToAscii= 0;
+    let textToAscii1=0;
+    let space = "";
+    texttoascii = text.charCodeAt(i);
+    if(textToAscii=== 32){
+      space += 32;
+      result += String.fromCharCode(textToAscii);
+    }
+    else if (textToAscii >= 65 && textToAscii <= 90){
+      textToAscii1=(textToAscii - 90 - offset) %26 - 90;
+      result += String.fromCharCode(textToAscii1);
+    }else{
+      alert("PORFAVOR INGRESA TU TEXTO SIN SIGNOS DE PUNTUACION");
+      return result;
+    }
+    return result;/*
+  };
+
+
+
+  /*function decode (offset,text){
+      let result ="";
+      for ( let i = 0; i< text.length; i++){
+          let textToAscii= 0;
+          let texToAscii1= 0;
+          let space1 = "";
+          textToAscii = text.charCodeAt(i);
+          if(textToAscii===32){
+            space1 += 32;
+            result += String.fromCharCode(textToAscii);
+          }
+               else if(textToAscii >= 65 && textToAscii <=90 ){
+                  textToAscii1= (textToAscii - 90 - offset) %26 + 90;
+                  result += String.fromCharCode(textToAscii1);
+             }else{
+                alert("PORFAVOR INGRESA TU TEXTO EN MAYUSCULAS, SIN SIGNOS DE PUNTUACIÓN")
+                return result;
+              }
+          }
+return result;
+}*/
