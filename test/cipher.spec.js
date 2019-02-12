d9d4781d72ab059ba98a29635207fb9ca7aed712
Macarena Terán <macarenateran.flores@gmail.com>
2019-02-12 20:30:23 -0300

    actialización del read me

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,7 +10,14 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', ()=>{
+      assert.equal(cipher.encode(33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ" ),"HIJKLMNOPQRSTUVWXYZABCDEFG" );
+    });
+
+    it('deberia retornar "RYVK WKMK" para "HOLA MACA" con offset de 10', ()=>{
+      assert.equal(cipher.encode(10,"HOLA MACA"),"RYVK WKMK");
+    });
+    
   });
 
   describe('cipher.decode', () => {
@@ -19,7 +26,19 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
-  });
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', ()=>{
+      assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+
+    it('deberia retornar "HOLA MACA" para "RYVK WKMK" con offset de 10', ()=>{
+      assert.equal(cipher.decode(10,"RYVK WKMK"),"HOLA MACA");
+
+    });
+   
+   });
+
+
+
+  
 
 });
